f9cb96939fa058020224f256ddaff963e9503f3b
jinge <jinge@wacai.com>
2017-10-16 04:33:35 +0800

    update

--- src/main/kotlin/com/laibao/kotlin/chapter01/HelloKotlin.kt
+++ src/main/kotlin/com/laibao/kotlin/chapter01/HelloKotlin.kt
@@ -2,8 +2,11 @@ package com.laibao.kotlin.chapter01
 
 fun main(args: Array<String>) {
     println("Hello,Kotlin")
-    val student = Student("laibao")
-    println("Student:${student.name}")
-    val carManufacturer = CarManufacturer("BenZi")
-    println("Car manufacture:${carManufacturer.name}")
+    println("Hello,Kotlin")
+    println("Hello,Kotlin")
+    println("Hello,Kotlin")
+    //val student = Student("laibao")
+    //println("Student:${student.name}")
+    //val carManufacturer = CarManufacturer("BenZi")
+    //println("Car manufacture:${carManufacturer.name}")
 }
