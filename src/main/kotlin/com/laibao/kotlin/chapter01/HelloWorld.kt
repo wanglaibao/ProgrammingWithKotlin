70b573ce9882b033784c5ad95789fc1812d64758
jinge <jinge@wacai.com>
2017-10-31 19:47:09 +0800

    add HelloWorld.kt file info

---
+++ src/main/kotlin/com/laibao/kotlin/chapter01/HelloWorld.kt
@@ -0,0 +1,11 @@
+package com.laibao.kotlin.chapter01
+
+/**
+ * Created by jinge on 2017/10/29.
+ */
+
+fun main(args: Array<String>): Unit {
+    println("my name is jinge")
+    println("i am 100 old")
+    println("i am still young")
+}
