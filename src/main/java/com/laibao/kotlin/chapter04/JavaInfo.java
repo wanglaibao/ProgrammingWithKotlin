1530dc4c1474956554b5844456660749fcf67fee
jinge <jinge@wacai.com>
2017-11-06 20:56:18 +0800

    add new kotlin and java files info

---
+++ src/main/java/com/laibao/kotlin/chapter04/JavaInfo.java
@@ -0,0 +1,10 @@
+package com.laibao.kotlin.chapter04;
+
+/**
+ * Created by jinge on 2017/11/6.
+ */
+public class JavaInfo {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/kotlin/com/laibao/kotlin/chapter04/KotlinInfo.kt
@@ -0,0 +1,8 @@
+package com.laibao.kotlin.chapter04
+
+/**
+ * Created by jinge on 2017/11/6.
+ */
+fun main(args: Array<String>) {
+
+}
