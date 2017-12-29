f71fed19bf0acb531224b96a3b0e038fc8a3c3b3
jinge <jinge@wacai.com>
2017-12-29 08:24:12 +0800

    update config file info

---
+++ src/main/java/com/laibao/kotlin/chapter01/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.laibao.kotlin.chapter01;
+
+/**
+ * @author laibao wang
+ * @date 2017-12-29
+ * @version 1.0
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+
+    }
+}
