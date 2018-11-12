91042273bbab3baa3b192b02385e58cfe37d6064
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-11-12 17:52:05 +0800

    update pom.xml file info

---
+++ src/main/kotlin/com/laibao/kotlin/oppinkotlin/visibility/DerivedContainer.kt
@@ -0,0 +1,8 @@
+package com.laibao.kotlin.oppinkotlin.visibility
+
+/**
+ * @author laibao wang
+ */
+class DerivedContainer : Container(){
+    public override val name: String = "Something else"
+}
