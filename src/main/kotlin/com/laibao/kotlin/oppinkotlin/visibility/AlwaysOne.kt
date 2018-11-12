b061e713b561a371553208c42e29f940d9fe04cf
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-11-12 20:07:29 +0800

    update pom.xml file info

---
+++ src/main/kotlin/com/laibao/kotlin/oppinkotlin/visibility/AlwaysOne.kt
@@ -0,0 +1,10 @@
+package com.laibao.kotlin.oppinkotlin.visibility
+
+/**
+ * @author laibao wang
+ */
+class AlwaysOne : DDerived (){
+    override fun someMethod(): Int {
+        return 1
+    }
+}
