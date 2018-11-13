d26e82c40575037ef4de96d6e9c9ba9616fe0673
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-11-14 02:03:25 +0800

    add a tech pdf file info

--- src/main/kotlin/com/laibao/kotlin/functions/SimpleFunExample.kt
+++ src/main/kotlin/com/laibao/kotlin/functions/SimpleFunExample.kt
@@ -59,3 +59,33 @@ fun parseInt(str : String?) : Int? {
     return str?.toInt()
 }
 
+fun positiveRoot(k: Int): Double {
+    require(k >= 0)
+    return Math.sqrt(k.toDouble())
+}
+
+fun negativeRoot(k: Int): Double {
+    require(k >= 0)
+    return -Math.sqrt(k.toDouble())
+}
+
+fun roots(k: Int): Array<Double> {
+    require(k >= 0)
+    val root = Math.sqrt(k.toDouble())
+    return arrayOf(root, -root)
+}
+
+
+data class Roots(val pos: Double, val neg: Double)
+
+fun roots2(k: Int): Roots {
+    require(k >= 0)
+    val root = Math.sqrt(k.toDouble())
+    return Roots(root, -root)
+}
+
+fun roots3(k: Int): Pair<Double, Double> {
+    require(k >= 0)
+    val root = Math.sqrt(k.toDouble())
+    return Pair(root, -root)
+}
---
+++ src/main/kotlin/com/laibao/kotlin/oppinkotlin/Submarine.kt
@@ -0,0 +1,14 @@
+package com.laibao.kotlin.oppinkotlin
+
+/**
+ * @author laibao wang
+ */
+class Submarine {
+    fun fire(): Unit {
+        println("Firing torpedoes")
+    }
+
+    fun submerge(): Unit {
+        println("Submerging")
+    }
+}
