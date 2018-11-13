98b5947639a3d44d4c43ca7a83b716a1f0c500d3
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-11-13 09:40:47 +0800

    update pom.xml file info

---
+++ src/main/kotlin/com/laibao/kotlin/functions/SimpleFunExample.kt
@@ -0,0 +1,18 @@
+package com.laibao.kotlin.functions
+
+/**
+ * @author laibao wang
+ */
+
+fun main(args: Array<String>) {
+
+}
+
+fun hello():String = "hello,Kotlin"
+
+fun hello(name: String, location: String) = "hello to you $name at $location"
+
+fun printlnOne(str: String) = println(str)
+
+fun printlnTwo(str: String) : Unit = println(str)
+
