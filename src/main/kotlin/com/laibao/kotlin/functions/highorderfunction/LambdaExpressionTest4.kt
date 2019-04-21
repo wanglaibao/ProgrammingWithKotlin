1e558e6200cd914b30328ce422ab82b517c6d71e
wanglaibao2010 <wanglaibao2010@gmail.com>
2019-04-21 14:09:37 +0800

    add a tech pdf file info

--- src/main/kotlin/com/laibao/kotlin/functions/highorderfunction/LambdaExpressionTest4.kt
+++ src/main/kotlin/com/laibao/kotlin/functions/highorderfunction/LambdaExpressionTest4.kt
@@ -4,6 +4,20 @@ fun main() {
     longOperationAsync(10,::hideProgress)
 
     longOperationAsync(100){ hideProgress()}
+
+    addLogs("Some operations"){
+        println("我来了你在哪里啊")
+    }
+
+    addLogs("Sleeper") {
+        Thread.sleep(1000)
+    }
+
+    //Usage
+    ifSupportsLolipop {
+        // Operation
+        println()
+    }
 }
 
 fun getAndFillList(onStart: () -> Unit = {}, onFinish: () -> Unit = {}){
@@ -22,6 +36,18 @@ fun hideProgress() {
 }
 
 
+fun addLogs(tag: String, block: () -> Unit) {
+    println("$tag started")
+    val startTime = System.currentTimeMillis()
+    block.invoke()
+    val endTime = System.currentTimeMillis()
+    println("$tag finished. It took " + (endTime - startTime))
+}
 
 
+fun ifSupportsLolipop(block: () -> Unit) {
 
+    if (true) {
+        block()
+    }
+}
